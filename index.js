e46d6910852a1cfeeb8f691357f58a967fe63097
Tatiana Ciocca <18190041+Tacial@users.noreply.github.com>
2022-12-01 15:53:44 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Olá, Mundo!')
+
+let a = 10;
+const b = 20;
+/*comparação comum*/
+console.log(a == b)
+/*comparação com tipos diferentes*/
+const c = "10";
+console.log(a == c)
+/*comparação comum E de tipo*/
+console.log(a === c)
